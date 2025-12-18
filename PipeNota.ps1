e021cdd867c0d7d9fd49b8e7c6ee85c0f0814a68
Fabiano Silva <fabiano.msrc@outlook.com>
2025-12-18 19:34:50 -0300

    Carregamento do módulo PipeNota

--- PipeNota.ps1
+++ PipeNota.ps1
@@ -2,3 +2,9 @@
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
+
+$modulePath = Join-Path $PSScriptRoot -ChildPath 'PipeNota.psm1'
+
+if (-not (Get-Module | Where-Object { $_.Path -eq $modulePath })) {
+    Import-Module -Name $modulePath -ErrorAction Stop
+}
