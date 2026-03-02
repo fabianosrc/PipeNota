e2e404a4320b5a017e5a4e0e88f747f5b6cca37a
Fabiano Silva <fabiano.msrc@outlook.com>
2026-03-02 19:34:57 -0300

    chore: add assinatura da função

--- src/Public/New-PipeNotaEmpresa.ps1
+++ src/Public/New-PipeNotaEmpresa.ps1
@@ -1,4 +1,39 @@
 ﻿function New-PipeNotaEmpresa {
-    [CmdletBinding()]
-    param ()
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Medium')]
+    param (
+        [Parameter(Mandatory)]
+        [string]$RazaoSocial,
+
+        [Parameter()]
+        [string]$NomeFantasia,
+
+        [Parameter(Mandatory)]
+        [string]$Cnpj,
+
+        [Parameter()]
+        [string]$Ie,
+
+        [Parameter()]
+        [ValidateSet('Matriz', 'Filial')]
+        [string]$Tipo,
+
+        [Parameter()]
+        [ValidateSet('Ativa', 'Inativa')]
+        [string]$Situacao,
+
+        [Parameter()]
+        [string]$DiretorioXmlAutorizados,
+
+        [Parameter()]
+        [string]$DiretorioXmlProcessados,
+
+        [Parameter()]
+        [string[]]$EmailPara,
+
+        [Parameter()]
+        [string[]]$EmailCc,
+
+        [Parameter()]
+        [string[]]$EmailCco
+    )
 }
