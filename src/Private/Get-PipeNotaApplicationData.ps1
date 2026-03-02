d975a95882e86e861e1f36aba9d5dc3afb05c49e
Fabiano Silva <fabiano.msrc@outlook.com>
2026-03-02 16:03:20 -0300

    chore: add função que retorna diretório base PipeNoa 'ApplicationData'

---
+++ src/Private/Get-PipeNotaApplicationData.ps1
@@ -0,0 +1,16 @@
+﻿function Get-PipeNotaApplicationData {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param ()
+
+    $localAppDataPath = [System.Environment]::GetFolderPath('LocalApplicationData')
+
+    $pipeNotaDataPath = [System.IO.Path]::Combine($localAppDataPath, 'PipeNota')
+
+    if (-not (Test-Path -LiteralPath $pipeNotaDataPath -PathType Container)) {
+        New-Item -Path $pipeNotaDataPath -ItemType Directory -Force | Out-Null
+    }
+
+    # Envia a saída para o Pipeline
+    Write-Output $pipeNotaDataPath
+}
