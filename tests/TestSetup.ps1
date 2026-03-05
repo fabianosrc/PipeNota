6063ac2fcfff285cb3e0d8d0a98dc132fe7d7d65
Fabiano Silva <fabiano.msrc@outlook.com>
2026-03-05 17:57:14 -0300

    test: add setup script to load src scripts

---
+++ tests/TestSetup.ps1
@@ -0,0 +1,5 @@
+﻿$srcPath = Join-Path -Path $PSScriptRoot -ChildPath '..\src'
+
+Get-ChildItem -Path $srcPath -Filter *.ps1 -Recurse -File |
+    Sort-Object FullName |
+    ForEach-Object { . $_.FullName }
