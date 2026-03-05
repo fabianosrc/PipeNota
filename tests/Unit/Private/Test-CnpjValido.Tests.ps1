27fa5286ca76a30711308a0a1619f60d6b40955f
Fabiano Silva <fabiano.msrc@outlook.com>
2026-03-05 18:34:31 -0300

    test: add teste para validação de CNPJ

---
+++ tests/Unit/Private/Test-CnpjValido.Tests.ps1
@@ -0,0 +1,34 @@
+﻿BeforeAll {
+    . "$PSScriptRoot/../../TestSetup.ps1"
+}
+
+Describe 'Test-CnpjValido' {
+
+    Context 'CNPJ válido' {
+        It 'Retorna True para CNPJ com máscara' {
+            Test-CnpjValido -Cnpj '78.332.372/0001-02' | Should -BeTrue
+        }
+
+        It 'Retorna True para CNPJ sem máscara' {
+            Test-CnpjValido -Cnpj '78332372000102' | Should -BeTrue
+        }
+    }
+
+    Context 'CNPJ inválido' {
+        It 'Retorna False para CNPJ com máscara' {
+            Test-CnpjValido -Cnpj '28.775.268/0001-26' | Should -BeFalse
+        }
+
+        It 'Retorna False para CNPJ com máscara' {
+            Test-CnpjValido -Cnpj '28775268000126' | Should -BeFalse
+        }
+
+        It 'Retorna False para CNPJ com números repetidos com máscara' {
+            Test-CnpjValido -Cnpj '11.111.111/1111-11' | Should -BeFalse
+        }
+
+        It 'Retorna False para CNPJ com números repetidos sem máscara' {
+            Test-CnpjValido -Cnpj '99999999999999' | Should -BeFalse
+        }
+    }
+}
