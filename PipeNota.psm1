3bd67c86e998542ae55f281bd76bea8d9717baf9
Fabiano Silva <fabiano.msrc@outlook.com>
2026-03-05 22:41:37 -0300

    autoload das funções via dot-sourcing

--- PipeNota.psm1
+++ PipeNota.psm1
@@ -2,3 +2,11 @@
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
+
+$srcPath = Join-Path -Path $PSScriptRoot -ChildPath 'src'
+
+Get-ChildItem -Path "$srcPath/Private" -File | ForEach-Object { . $_.FullName }
+
+Get-ChildItem -Path "$srcPath/Public" -File | ForEach-Object { . $_.FullName }
+
+Export-ModuleMember -Function New-PipeNotaEmpresa, Get-PipeNotaEmpresa, Set-PipeNotaEmpresa, Remove-PipeNotaEmpresa
