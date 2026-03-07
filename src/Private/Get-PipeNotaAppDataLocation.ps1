fd0bd9de14fe37236700dfce60cd769f2557fe3d
Fabiano Silva <fabiano.msrc@outlook.com>
2026-03-07 14:44:34 -0300

    chore: add comment-based help

--- src/Private/Get-PipeNotaAppDataLocation.ps1
+++ src/Private/Get-PipeNotaAppDataLocation.ps1
@@ -1,4 +1,17 @@
-﻿function Get-PipeNotaAppDataLocation {
+﻿<#
+.SYNOPSIS
+Obtém o caminho da pasta de dados do PipeNota em LocalAppData.
+
+.DESCRIPTION
+Retorna o caminho da pasta "PipeNota" dentro do diretório
+LocalApplicationData do usuário. Caso a pasta não exista,
+ela será criada automaticamente.
+
+.OUTPUTS
+System.String
+Caminho completo da pasta de dados do PipeNota.
+#>
+function Get-PipeNotaAppDataLocation {
     [CmdletBinding()]
     [OutputType([string])]
     param ()
