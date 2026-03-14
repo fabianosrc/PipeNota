d9721dd33b79035f4b96d446b7afafcfb1e25b50
Fabiano Silva <fabiano.msrc@outlook.com>
2026-03-14 16:28:02 -0300

    feat: return an ISO 8601 timestamp

---
+++ src/Private/Get-CurrentTimestamp.ps1
@@ -0,0 +1,31 @@
+﻿<#
+.SYNOPSIS
+Retorna o timestamp atual no formato ISO 8601 com offset do timezone.
+
+.DESCRIPTION
+Gera um timestamp no formato ISO 8601 (yyyy-MM-ddTHH:mm:sszzz) com o
+offset de timezone.
+
+.PARAMETER Format
+Formato de saída para formatos customizáveis.
+
+.EXAMPLE
+PS C:\> Get-CurrentTimestamp
+
+.OUTPUTS
+System.String
+#>
+function Get-CurrentTimestamp {
+    [CmdletBinding()]
+    [OutputType([string])]
+    param (
+        [Parameter()]
+        [string]$Format
+    )
+
+    if (-not ($Format)) {
+        $Format = 'yyyy-MM-ddTHH:mm:sszzz'
+    }
+
+    (Get-Date).ToString($Format)
+}
