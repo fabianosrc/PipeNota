51d9d3e9ce75b995ed83c46416950f32b439fc9d
Fabiano Silva <fabiano.msrc@outlook.com>
2026-03-14 13:02:22 -0300

    chore: add Dot-sourcing import functions

--- PipeNota.psm1
+++ PipeNota.psm1
@@ -2,3 +2,18 @@
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
+
+$modulePath = @{
+    Private = [System.IO.Path]::Combine($PSScriptRoot, 'src', 'Private')
+    Public  = [System.IO.Path]::Combine($PSScriptRoot, 'src', 'Public')
+}
+
+# Dot-sourcing private functions (not exported)
+Get-ChildItem -Path $modulePath.Private -Filter *.ps1 -File | ForEach-Object {
+    . $_.FullName
+}
+
+# Dot-sourcing public functions (exported)
+Get-ChildItem -Path $modulePath.Public -Filter *.ps1 -File | ForEach-Object {
+    . $_.FullName
+}
